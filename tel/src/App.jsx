ef7056c8c36e1cce1778e6c44adee4b77908ab2d
Keti Yohannes <ketiyohannes@gmail.com>
2023-08-31 09:30:14 +0000

    rest

--- tel/src/App.jsx
+++ tel/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
     useEffect(() => {
-      window.open("sms:+251939858566")
+      window.location.href = "sms:+251939858566"
   }, [])
   
   
